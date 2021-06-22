ac46e107a7c242da535ac5a6464134c2ddb50bc4
Victor Shevchuk <v.shevchuk@viseven.com>
2021-06-22 08:18:27 +0400

    first commit

---
+++ src/UI/Modal.js
@@ -0,0 +1,10 @@
+class Modal {
+  constructor(contentId) {
+    this.contentTemplateEl = document.getElementById(contentId);
+    this.modalTemplateEl = document.getElementById('modal-template');
+  }
+
+  show() {}
+
+  hide() {}
+}
